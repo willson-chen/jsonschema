8aa7b61d9ab4c711331e7a67907022dc73e04919
heaven-hq <51770115+heaven-hq@users.noreply.github.com>
2020-07-08 16:11:14 +0800

    Update validators.py

--- jsonschema/validators.py
+++ jsonschema/validators.py
@@ -333,7 +333,7 @@ def create(
                             instance=instance,
                             schema=_schema,
                         )
-                        if k not in {u"if", u"$ref"}:
+                        if k not in {u"if", u"$ref",u"additionalProperties"}:
                             error.schema_path.appendleft(k)
                         yield error
             finally:
